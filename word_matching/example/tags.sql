5f4bde6f766ab0dcf16e0f0740cca43d6dd49875
rikumomo <92307653+rikumomo0407@users.noreply.github.com>
2024-03-06 06:46:55 +0900

    Update tags.sql

--- word_matching/example/tags.sql
+++ word_matching/example/tags.sql
@@ -11,31 +11,11 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- データベース: `php_test`
---
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `tags`
---
-
 CREATE TABLE `tags` (
   `id` int NOT NULL,
   `name` text NOT NULL,
   `amount` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- テーブルのデータのダンプ `tags`
---
 
 INSERT INTO `tags` (`id`, `name`, `amount`) VALUES
 (1, 'Python', 79071),
@@ -5061,7 +5041,3 @@ ALTER TABLE `tags`
 ALTER TABLE `tags`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5001;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
